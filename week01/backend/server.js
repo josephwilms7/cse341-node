20558744039c7dbcafa37d9ad231101d8c1db8f5
josephwilms7 <josephwilmsdev@gmail.com>
2025-01-18 13:45:49 -0700

    Yes

--- week01/backend/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/', require('./routes'));
-
-app.listen(process.env.port || 3000);
-console.log("Web server is running on port" + (process.env.port || 3000)); 
