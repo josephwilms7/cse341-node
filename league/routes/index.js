4a5e70f1f576a0d466c53da646e4f3a9883b01a0
josephwilms7 <josephwilmsdev@gmail.com>
2025-02-01 16:17:41 -0700

    Initial Project Copy

---
+++ league/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/', require('./swagger'));
+
+router.use('/users', require('./users'));
+
+module.exports = router;
